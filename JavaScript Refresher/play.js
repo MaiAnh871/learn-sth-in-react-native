0e3e21c7a398d242bd35a131d235cf4ef3dee864
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-11-13 20:42:42 +0700

    arrow functions

--- JavaScript Refresher/play.js
+++ JavaScript Refresher/play.js
@@ -1,3 +1,4 @@
+/*
 var name = 'Max';
 var age = 29;
 var hasHobbies = true;
@@ -11,5 +12,30 @@ function summarizeUser(userName, userAge, userHasHobbies) {
         ' and the user has hobbies: ' + userHasHobbies
     );
 }
+
+console.log(summarizeUser(name, age, hasHobbies));
+*/
+
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+const summarizeUser = (userName, userAge, userHasHobbies) => {
+    return (
+        'Name is ' + userName +
+        ', age is ' + userAge + 
+        ' and the user has hobbies: ' + userHasHobbies
+    );
+};
+
+// const add = (a, b) => a + b;
+// const addOne = (a) => a + 1;
+const addRandom = () => 1 + 2;
+
+// console.log(add(1, 2));
+// console.log(addOne(1));
+console.log(addRandom());
 
 console.log(summarizeUser(name, age, hasHobbies));
