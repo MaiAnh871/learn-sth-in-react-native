da19ee759f995df4552ebeaf22d4329e5d27d5f4
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-11-13 21:50:32 +0700

    arrays, objects and reference types

--- JavaScript Refresher/objects-properties&method.js
+++ JavaScript Refresher/objects-properties&method.js
@@ -14,6 +14,9 @@ const hobbies = ['Sport', 'Cooking'];
 for (let hobby of hobbies) {
     console.log(hobby);
 }*/
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+hobbies.push('Programming');
 console.log(hobbies);
 
