1b77422bf89695fd39e15592aa6e8df955077f5b
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-11-13 21:59:19 +0700

    spread operator & rest parameter

--- JavaScript Refresher/objects-properties&method.js
+++ JavaScript Refresher/objects-properties&method.js
@@ -13,10 +13,34 @@ const hobbies = ['Sport', 'Cooking'];
 /*
 for (let hobby of hobbies) {
     console.log(hobby);
-}*/
+}
 // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
 // console.log(hobbies);
 
 hobbies.push('Programming');
 console.log(hobbies);
+*/
+
+// const copiedArray = hobbies.slice();
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
+/*
+const toArray = (arg1, arg2, arg3) => {
+    return [arg1, arg2, arg3];
+};
+
+console.log(toArray (1, 2, 3));
+*/
+
+const toArray = (...args) => {
+    return args;
+};
+
+console.log(toArray (1, 2, 3, 4));
+
+
 
