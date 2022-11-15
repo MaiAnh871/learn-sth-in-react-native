45dc836109f6950db64a2422a7304bb5a8f1af00
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-11-15 09:24:17 +0700

    working with core components

---
+++ React Native Basics/RNCOURSE/App.js
@@ -0,0 +1,23 @@
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>     
+        <Text>Another piece of text!</Text>
+      </View>
+      <Text>Hello World, I am Mai Anh!</Text>
+      <Button title="Tap me!" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
