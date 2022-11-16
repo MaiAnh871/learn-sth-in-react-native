e7e097c76368b16a65007803072351638899e167
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-11-16 09:12:14 +0700

    Fetching BACKEND data

--- Sending HTTP Requests/screens/RecentExpenses.js
+++ Sending HTTP Requests/screens/RecentExpenses.js
@@ -1,11 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpense } from '../util/http';
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpense();
+    }
+
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
--- Sending HTTP Requests/util/http.js
+++ Sending HTTP Requests/util/http.js
@@ -1,8 +1,33 @@
 import axios from 'axios';
 
+const BACKEND_URL = 'https://react-native-course-cc4f3-default-rtdb.asia-southeast1.firebasedatabase.app'
+
 export function storeExpense(expenseData) {
     axios.post(
-        'https://react-native-course-cc4f3-default-rtdb.asia-southeast1.firebasedatabase.app/expense.json',
+        BACKEND_URL + '/expense.json',
         expenseData
     );
+}
+
+export async function fetchExpense() {
+    const response = await axios.get(
+        BACKEND_URL + '/expense.json',
+    );
+
+    const expenses = [];
+
+    //console.log(response.data);
+
+    for (const key in response.data) {
+        const expenseObj = {
+            id: key,
+            amount: response.data[key].amount,
+            date: new Date(response.data[key].date),
+            description: response.data[key].description
+        };
+
+        expenses.push(expenseObj);
+    }
+
+    return expenses;
 }
