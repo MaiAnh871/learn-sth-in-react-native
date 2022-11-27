a0b9ed366bec5fe4126e4a747eb02aad405829f9
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-11-27 14:29:20 +0700

    exploring layout & flexbox

---
+++ React Native Basics/exploring-layout/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add Goal' />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  }
+
+});
