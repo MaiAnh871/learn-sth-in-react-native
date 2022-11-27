e8fee82a0331404133b5d667f0d1e262412437db
Mai Anh <94096485+MaiAnh871@users.noreply.github.com>
2022-11-27 14:39:56 +0700

    using flex box to create layout

--- React Native Basics/exploring-layout/App.js
+++ React Native Basics/exploring-layout/App.js
@@ -4,8 +4,8 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!' />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal!' />
         <Button title='Add Goal' />
       </View>
       <View>
@@ -18,6 +18,16 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
   }
-
 });
